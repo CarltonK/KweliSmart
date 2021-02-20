3d318ed0486832a3edf8282450bb73534c59fb9c
Mark <mcarlton33@gmail.com>
2021-02-20 21:39:01 +0300

    project modularization

---
+++ functions/src/handler/HttpHandler.ts
@@ -0,0 +1,29 @@
+import { Logger } from '@firebase/logger';
+import * as functions from 'firebase-functions';
+
+export default class HttpHandler {
+
+    private logger: Logger = new Logger('HttpHandler');
+
+    constructor() {
+        this.logger.setLogLevel('debug')
+    }
+
+    public async handleRequest(request: functions.https.Request, response: functions.Response<any>): Promise<void> {
+        this.logger.log(request.path, request.method);
+        const { method } = request;
+
+         // return 405 METHOD NOT ALLOWED
+        if (method !== 'POST') { response.status(405).send({
+            status: false,
+            detail: `${method.toUpperCase()} not allowed`
+        }); return; };
+
+        // return 404 NOT FOUND
+        response.status(404).send({
+            status: false,
+            detail: 'The resource you requested is not available'
+        });
+        return;
+    }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,21 @@
 import * as functions from 'firebase-functions';
+import { Logger } from '@firebase/logger';
+import HttpHandler from './handler/HttpHandler';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const logger = new Logger('Root');
+logger.setLogLevel('debug');
+
+// Define functions region
+const regionalFunctions = functions.runWith({
+  timeoutSeconds: 60,
+  memory: '512MB'
+}).region('europe-west3');
+
+
+const GlobalHttpHandler = new HttpHandler();
+
+/******************
+* Http Trigger
+******************/
+
+export const api = regionalFunctions.https.onRequest(GlobalHttpHandler.handleRequest.bind(GlobalHttpHandler));
