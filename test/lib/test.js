b3fbd2fb61cc303442c0392deb06a07ac812af64
Mark <mcarlton33@gmail.com>
2021-04-06 10:52:34 +0200

    define tests

--- test/lib/test.js
+++ test/lib/test.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const assert = require("assert");
 const firebase = require("@firebase/rules-unit-testing");
 const MY_PROJECT_ID = 'kwelismart';
 /**
@@ -12,26 +11,113 @@ const myAuth = { uid: myId, email: myEmail };
 /**
  * THEIR AUTH
 */
-// const theirId = 'user_xyz';
-// const theirEmail = 'userxyz@email.com';
-// const theirAuth = { uid: theirId, email: theirEmail };
+const theirId = 'user_xyz';
+const theirEmail = 'userxyz@email.com';
+const theirAuth = { uid: theirId, email: theirEmail };
 const getFirestore = (auth) => {
     return firebase.initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth }).firestore();
 };
-// const getAdminFirestore = () => {
-//     return firebase.initializeTestApp({ projectId: MY_PROJECT_ID }).firestore();
-// };
+const getAdminFirestore = () => {
+    return firebase.initializeAdminApp({ projectId: MY_PROJECT_ID }).firestore();
+};
 beforeEach(async () => {
     await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
 });
 describe('KweliSmart', () => {
-    it('Can perform basic arithmetic', () => {
-        assert.strictEqual(1 + 1, 2);
+    /**
+     * USER GET OPERATIONS *
+    */
+    it('Can\'t allow an unauthenticated user to get a users document', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.get());
     });
-    it('Can\'t delete a users document', async () => {
+    it('Can\'t allow a user to get another users document', async () => {
+        const db = getFirestore(theirAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.get());
+    });
+    it('Can\'t allow an unauthenticated user to get all documents in the users collection', async () => {
+        const db = getFirestore();
+        await firebase.assertFails(db.collection('users').get());
+    });
+    it('Can\'t allow a user to get all documents in the users collection', async () => {
+        const db = getFirestore(myAuth);
+        await firebase.assertFails(db.collection('users').get());
+    });
+    it('Can\'t allow an unauthenticated user to get their document', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.get());
+    });
+    it('Can allow a user to get their document', async () => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertSucceeds(testDoc.get());
+    });
+    /**
+     * USER CREATE OPERATIONS *
+    */
+    it('Can\'t allow an unauthenticated user to create their document', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
+    });
+    it('Can\'t allow a user to create another users document', async () => {
+        const db = getFirestore(theirAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
+    });
+    it('Can allow a user to create their document', async () => {
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertSucceeds(testDoc.set({ foo: 'bar' }));
+    });
+    /**
+     * USER UPDATE OPERATIONS *
+    */
+    it('Can\'t allow an unauthenticated user to update a document', async () => {
+        const admin = getAdminFirestore();
+        await admin.collection('users').doc(myId).set({ foo: 'bar' });
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.update({ foo: 'not bar' }));
+    });
+    it('Can\'t allow a user to update another users document', async () => {
+        const admin = getAdminFirestore();
+        await admin.collection('users').doc(myId).set({ foo: 'bar' });
+        const db = getFirestore(theirAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.update({ foo: 'not bar' }));
+    });
+    it('Can allow a user to update their document', async () => {
+        const admin = getAdminFirestore();
+        await admin.collection('users').doc(myId).set({ foo: 'bar' });
+        const db = getFirestore(myAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertSucceeds(testDoc.update({ foo: 'not bar' }));
+    });
+    /**
+     * USER DELETE OPERATIONS *
+    */
+    it('Can\'t allow an unauthenticated user to delete their document', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.delete());
+    });
+    it('Can\'t allow a user to delete another users document', async () => {
+        const db = getFirestore(theirAuth);
+        const testDoc = db.collection('users').doc(myId);
+        await firebase.assertFails(testDoc.delete());
+    });
+    it('Can\'t allow a user to delete their document', async () => {
         const db = getFirestore(myAuth);
         const testDoc = db.collection('users').doc(myId);
         await firebase.assertFails(testDoc.delete());
     });
+});
+after(async () => {
+    await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
+    await Promise.all(firebase.apps().map(app => app.delete()));
 });
 //# sourceMappingURL=test.js.map
