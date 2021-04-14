2700e61ece76f8ea657a608b4b984137a2d8221f
Mark <mcarlton33@gmail.com>
2021-04-14 09:54:37 +0200

    modularize security rules testing

--- test/src/handler/users-user.ts
+++ test/src/handler/users-user.ts
@@ -28,32 +28,117 @@ export default class UserDocumentHandler {
         return firebase.initializeAdminApp({ projectId: this.projectId }).firestore();
     };
 
+    async beforeEachCallback() {
+        await firebase.clearFirestoreData({ projectId: this.projectId });
+    }
+
+    async afterAllCallback() {
+        await firebase.clearFirestoreData({ projectId: this.projectId });
+        await Promise.all(firebase.apps().map(app => app.delete()));
+    }
+
 
     /**
      * USER GET OPERATIONS *
      */
 
-
-    async getUnauthenticated(): Promise<any> {
+    async denyGetUnauthenticated() {
         const db = this.getFirestore();
         const testDoc = db.collection('users').doc(this.myId);
         await firebase.assertFails(testDoc.get());
     }
 
-    async getOthersDocument(): Promise<any> {
+    async denyGetOthers() {
         const db = this.getFirestore(this.theirAuth);
         const testDoc = db.collection('users').doc(this.myId);
         await firebase.assertFails(testDoc.get());
     }
 
-    async listUsersCollection(): Promise<any> {
+    async denyListUsers() {
         const db = this.getFirestore(this.myAuth);
         await firebase.assertFails(db.collection('users').get());
     }
 
-    async listUsersCollectionUnauthenticated(): Promise<any> {
+    async denyListUsersUnauthenticated() {
         const db = this.getFirestore();
         await firebase.assertFails(db.collection('users').get());
+    }
+
+    async allowGetOwn() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertSucceeds(testDoc.get());
+    }
+
+
+    /**
+     * USER CREATE OPERATIONS *
+     */
+
+    async denyCreateUnauthenticated() {
+        const db = this.getFirestore();
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
+    }
+
+    async denyCreateOthers() {
+        const db = this.getFirestore(this.theirAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
+    }
+
+    async denyCreateNoRequiredFields() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
+    }
+
+    async denyCreateNatIdLess() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ nationalIdNumber: '123456' }));
+    }
+
+    async denyCreateNatIdMore() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ nationalIdNumber: '123456789' }));
+    }
+
+    async denyCreateNatIdNotString() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ nationalIdNumber: 123456789 }));
+    }
+
+    async denyCreatePhoneNotString() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ phoneNumber: 1234567890 }));
+    }
+
+    async denyCreatePhoneNot10Digits() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ phoneNumber: '123456789' }));
+    }
+
+    async denyCreatePhoneIncorrectStartDigits() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ phoneNumber: '123456789' }));
+    }
+
+    async denyCreateMissingRequiredField() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.set({ nationalIdNumber: '12345678', emailAddress: this.myEmail }));
+    }
+
+    async allowCreateOwn() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertSucceeds(testDoc.set({ phoneNumber: '0712345678', nationalIdNumber: '12345678', emailAddress: this.myEmail }));
     }
 
 
@@ -61,14 +146,63 @@ export default class UserDocumentHandler {
      * USER UPDATE OPERATIONS *
      */
 
-
-
-    async updateUnauthenticated(): Promise<any> {
+    async denyUpdateUnauthenticated() {
         const admin = this.getAdminFirestore();
         await admin.collection('users').doc(this.myId).set({ foo: 'bar' });
 
         const db = this.getFirestore();
         const testDoc = db.collection('users').doc(this.myId);
         await firebase.assertFails(testDoc.update({ foo: 'not bar' }));
+    }
+
+    async denyUpdateOthers() {
+        const admin = this.getAdminFirestore();
+        await admin.collection('users').doc(this.myId).set({ foo: 'bar' });
+
+        const db = this.getFirestore(this.theirAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.update({ foo: 'not bar' }));
+    }
+
+    async denySomeFieldsUpdate() {
+        const admin = this.getAdminFirestore();
+        await admin.collection('users').doc(this.myId).set({ foo: 'bar', phoneNumber: '0712345678' });
+
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.update({ foo: 'not bar', phoneNumber: '0787654321' }));
+    }
+
+    async denyUpdateOwn() {
+        const admin = this.getAdminFirestore();
+        await admin.collection('users').doc(this.myId).set({ foo: 'bar' });
+
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertSucceeds(testDoc.update({ foo: 'not bar' }));
+    }
+
+
+    /**
+     * USER DELETE OPERATIONS *
+     */
+
+
+     async denyDeleteUnauthenticated() {
+        const db = this.getFirestore();
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.delete());
+    }
+
+    async denyDeleteOthers() {
+        const db = this.getFirestore(this.theirAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.delete());
+    }
+
+    async denyDeleteOwn() {
+        const db = this.getFirestore(this.myAuth);
+        const testDoc = db.collection('users').doc(this.myId);
+        await firebase.assertFails(testDoc.delete());
     }
 }
--- test/src/test.ts
+++ test/src/test.ts
@@ -1,35 +1,8 @@
-import * as firebase from '@firebase/rules-unit-testing';
 import UserDocumentHandler from './handler/users-user';
 
 const GlobalUserDocumentHandler = new UserDocumentHandler();
 
-const MY_PROJECT_ID = '<PROJECT_ID>';
-
-/**
- * MY AUTH
-*/
-const myId = 'user_abc';
-const myEmail = 'userabc@email.com';
-const myAuth = { uid: myId, email: myEmail };
-
-/**
- * THEIR AUTH
-*/
-const theirId = 'user_xyz';
-const theirEmail = 'userxyz@email.com';
-const theirAuth = { uid: theirId, email: theirEmail };
-
-const getFirestore = (auth?: Record<string, unknown>) => {
-    return firebase.initializeTestApp({ projectId: MY_PROJECT_ID, auth: auth }).firestore();
-};
-
-const getAdminFirestore = () => {
-    return firebase.initializeAdminApp({ projectId: MY_PROJECT_ID }).firestore();
-};
-
-beforeEach(async () => {
-    await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
-});
+beforeEach(GlobalUserDocumentHandler.beforeEachCallback.bind(GlobalUserDocumentHandler));
 
 describe('KweliSmart', () => {
 
@@ -39,106 +12,88 @@ describe('KweliSmart', () => {
 
     it(
         'Can\'t allow an unauthenticated user to get a users document',
-        GlobalUserDocumentHandler.getUnauthenticated.bind(GlobalUserDocumentHandler),
+        GlobalUserDocumentHandler.denyGetUnauthenticated.bind(GlobalUserDocumentHandler),
     );
 
     it(
         'Can\'t allow a user to get another users document',
-        GlobalUserDocumentHandler.getOthersDocument.bind(GlobalUserDocumentHandler),
+        GlobalUserDocumentHandler.denyGetOthers.bind(GlobalUserDocumentHandler),
     );
 
     it(
         'Can\'t allow an unauthenticated user to get all documents in the users collection',
-        GlobalUserDocumentHandler.listUsersCollectionUnauthenticated.bind(GlobalUserDocumentHandler),
+        GlobalUserDocumentHandler.denyListUsersUnauthenticated.bind(GlobalUserDocumentHandler),
     );
 
     it(
         'Can\'t allow a user to get all documents in the users collection',
-        GlobalUserDocumentHandler.listUsersCollection.bind(GlobalUserDocumentHandler),
+        GlobalUserDocumentHandler.denyListUsers.bind(GlobalUserDocumentHandler),
     );
 
-    it('Can\'t allow an unauthenticated user to get their document', async () => {
-        const db = getFirestore();
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.get());
-    });
-
-    it('Can allow a user to get their document', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertSucceeds(testDoc.get());
-    });
+    it(
+        'Can allow a user to get their document',
+        GlobalUserDocumentHandler.allowGetOwn.bind(GlobalUserDocumentHandler),
+    );
 
 
     /**
      * USER CREATE OPERATIONS *
      */
 
-    it('Can\'t allow an unauthenticated user to create their document', async () => {
-        const db = getFirestore();
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
-    });
+    it(
+        'Can\'t allow an unauthenticated user to create their document',
+        GlobalUserDocumentHandler.denyCreateUnauthenticated.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create another users document', async () => {
-        const db = getFirestore(theirAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
-    });
+    it(
+        'Can\'t allow a user to create another users document',
+        GlobalUserDocumentHandler.denyCreateOthers.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if required fields are missing', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ foo: 'bar' }));
-    });
+    it(
+        'Can\'t allow a user to create their document if required fields are missing',
+        GlobalUserDocumentHandler.denyCreateNoRequiredFields.bind(GlobalUserDocumentHandler),  
+    );
 
-    it('Can\'t allow a user to create their document if nationalId is less than 7 digits', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ nationalIdNumber: '123456' }));
-    });
+    it(
+        'Can\'t allow a user to create their document if nationalId is less than 7 digits',
+        GlobalUserDocumentHandler.denyCreateNatIdLess.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if nationalId is greater than 8 digits', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ nationalIdNumber: '123456789' }));
-    });
+    it(
+        'Can\'t allow a user to create their document if nationalId is greater than 8 digits',
+        GlobalUserDocumentHandler.denyCreateNatIdMore.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if nationalId is not a string', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ nationalIdNumber: 123456789 }));
-    });
+    it(
+        'Can\'t allow a user to create their document if nationalId is not a string',
+        GlobalUserDocumentHandler.denyCreateNatIdNotString.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if phoneNumber is not a string', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ phoneNumber: 1234567890 }));
-    });
+    it(
+        'Can\'t allow a user to create their document if phoneNumber is not a string',
+        GlobalUserDocumentHandler.denyCreatePhoneNotString.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if phoneNumber is not 10 digits', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ phoneNumber: '123456789' }));
-    });
+    it(
+        'Can\'t allow a user to create their document if phoneNumber is not 10 digits',
+        GlobalUserDocumentHandler.denyCreatePhoneNot10Digits.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to create their document if phoneNumber does not start with 07 or 01', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ phoneNumber: '123456789' }));
-    });
+    it(
+        'Can\'t allow a user to create their document if phoneNumber does not start with 07 or 01',
+        GlobalUserDocumentHandler.denyCreatePhoneIncorrectStartDigits.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can allow a user to create their document if a required field is missing', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.set({ nationalIdNumber: '12345678', emailAddress: myEmail }));
-    });
+    it(
+        'Can allow a user to create their document if a required field is missing',
+        GlobalUserDocumentHandler.denyCreateMissingRequiredField.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can allow a user to create their document if it is valid', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertSucceeds(testDoc.set({ phoneNumber: '0712345678', nationalIdNumber: '12345678', emailAddress: myEmail }));
-    });
+    it(
+        'Can allow a user to create their document if it is valid',
+        GlobalUserDocumentHandler.allowCreateOwn.bind(GlobalUserDocumentHandler),
+    );
 
 
     /**
@@ -148,35 +103,23 @@ describe('KweliSmart', () => {
 
     it(
         'Can\'t allow an unauthenticated user to update a document',
-        GlobalUserDocumentHandler.updateUnauthenticated.bind(GlobalUserDocumentHandler),
+        GlobalUserDocumentHandler.denyUpdateUnauthenticated.bind(GlobalUserDocumentHandler),
     );
 
-    it('Can\'t allow a user to update another users document', async () => {
-        const admin = getAdminFirestore();
-        await admin.collection('users').doc(myId).set({ foo: 'bar' });
+    it(
+        'Can\'t allow a user to update another users document',
+        GlobalUserDocumentHandler.denyUpdateOthers.bind(GlobalUserDocumentHandler),  
+    );
 
-        const db = getFirestore(theirAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.update({ foo: 'not bar' }));
-    });
+    it(
+        'Can\'t allow a user to update some fields in their document',
+        GlobalUserDocumentHandler.denySomeFieldsUpdate.bind(GlobalUserDocumentHandler), 
+    );
 
-    it('Can\'t allow a user to update some fields in their document', async () => {
-        const admin = getAdminFirestore();
-        await admin.collection('users').doc(myId).set({ foo: 'bar', phoneNumber: '0712345678' });
-
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.update({ foo: 'not bar', phoneNumber: '0787654321' }));
-    });
-
-    it('Can allow a user to update their document', async () => {
-        const admin = getAdminFirestore();
-        await admin.collection('users').doc(myId).set({ foo: 'bar' });
-
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertSucceeds(testDoc.update({ foo: 'not bar' }));
-    });
+    it(
+        'Can allow a user to update their document',
+        GlobalUserDocumentHandler.denyUpdateOwn.bind(GlobalUserDocumentHandler),
+    );
 
 
     /**
@@ -184,26 +127,20 @@ describe('KweliSmart', () => {
      */
 
 
-    it('Can\'t allow an unauthenticated user to delete their document', async () => {
-        const db = getFirestore();
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.delete());
-    });
+    it(
+        'Can\'t allow an unauthenticated user to delete their document',
+        GlobalUserDocumentHandler.denyDeleteUnauthenticated.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to delete another users document', async () => {
-        const db = getFirestore(theirAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.delete());
-    });
+    it(
+        'Can\'t allow a user to delete another users document',
+        GlobalUserDocumentHandler.denyDeleteOthers.bind(GlobalUserDocumentHandler),
+    );
 
-    it('Can\'t allow a user to delete their document', async () => {
-        const db = getFirestore(myAuth);
-        const testDoc = db.collection('users').doc(myId);
-        await firebase.assertFails(testDoc.delete());
-    });
+    it(
+        'Can\'t allow a user to delete their document',
+        GlobalUserDocumentHandler.denyDeleteOwn.bind(GlobalUserDocumentHandler),
+    );
 });
 
-after(async () => {
-    await firebase.clearFirestoreData({ projectId: MY_PROJECT_ID });
-    await Promise.all(firebase.apps().map(app => app.delete()));
-});
+after(GlobalUserDocumentHandler.afterAllCallback.bind(GlobalUserDocumentHandler));
