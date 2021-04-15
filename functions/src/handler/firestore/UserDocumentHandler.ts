d7cd9809b5169ac7ad837dc0417e2649c7dc2578
Mark <mcarlton33@gmail.com>
2021-04-15 17:17:10 +0200

    updated file name and path

--- functions/src/handler/firestore/UserDocumentHandler.ts
+++ functions/src/handler/firestore/UserDocumentHandler.ts
@@ -1,14 +1,16 @@
 import { Logger } from '@firebase/logger';
 import * as functions from 'firebase-functions';
 import { firestore } from 'firebase-admin';
+import NotificationHelper from '../../helper/NotificationHelper';
 
-export default class DocumentHandler {
+export default class UserDocumentHandler {
 
-    private logger: Logger = new Logger('DocumentHandler');
+    private logger: Logger = new Logger('UserDocumentHandler');
     private db = firestore();
+    private notificationHelper: NotificationHelper = new NotificationHelper();
 
     constructor() {
-        this.logger.setLogLevel('debug')
+        this.logger.setLogLevel('debug');
     }
 
     async newUserDocumentHandler(snapshot: firestore.QueryDocumentSnapshot, context: functions.EventContext) {
@@ -18,7 +20,15 @@ export default class DocumentHandler {
             const uid: string = snapshot.id;
             const userDocRef: firestore.DocumentReference = this.db.collection('users').doc(uid);
             const createdAt: firestore.Timestamp = firestore.Timestamp.now();
+            const { deviceToken } = snapshot.data();
 
+            if (deviceToken) {
+                await this.notificationHelper.singleNotificationSend(
+                    deviceToken,
+                    'Welcome to kweliscore',
+                    'Glad to have you on board'
+                );
+            }
             // this.logger.info(`The user identified by ${uid} was created at ${createdAt.toDate()}`);
             await userDocRef.update({ createdAt: createdAt });
         } catch (error) {
